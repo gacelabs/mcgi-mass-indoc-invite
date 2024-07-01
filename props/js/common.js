af288b27cf2c1c3b04c290276be3299cd8816c61
Eddie <eddie@annix.com.au>
2024-07-01 13:54:06 +0800

    refactor scripts

---
+++ props/js/common.js
@@ -0,0 +1,173 @@
+function addDaysToDate(date, days) {
+	var result = new Date(date);
+	result.setDate(result.getDate() + days);
+	return result;
+}
+
+function formatDateToFJY(date) {
+	var options = { month: 'short', day: 'numeric', year: 'numeric' };
+	return new Intl.DateTimeFormat('en-US', options).format(date);
+}
+
+function countMonths(startDate) {
+	var today = new Date();
+	var start = new Date(startDate);
+
+	var startYear = start.getFullYear();
+	var startMonth = start.getMonth();
+
+	var todayYear = today.getFullYear();
+	var todayMonth = today.getMonth();
+
+	var monthsDifference = (todayYear - startYear) * 12 + (todayMonth - startMonth);
+
+	return monthsDifference;
+}
+
+function addDotAfterThirdCharacter(str) {
+	return str.slice(0, 3) + '.' + str.slice(3);
+}
+
+function reqNotification(title, body, redirectUrl) {
+	Notification.requestPermission().then(function (permission) {
+		if (permission === 'granted') {
+			navigator.serviceWorker.getRegistrations().then(function (reg) {
+				if (reg && reg.length) {
+					for (var key in reg) {
+						if (Object.hasOwnProperty.call(reg, key)) {
+							var registration = reg[key];
+							if (registration.scope == 'https://gacelabs.github.io/mcgi-mass-indoc-invite/props/js/') {
+								var options = {
+									body: body,
+									icon: '/mcgi-mass-indoc-invite/props/images/logo.png',
+									data: {
+										redirectUrl: redirectUrl // Pass the redirect URL to the notification data
+									}
+								};
+								registration.showNotification(title, options)
+									.then(function () {
+										console.warn('Notification displayed successfully');
+									})
+									.catch(function (error) {
+										console.warn('Error displaying notification:', error);
+										document.getElementsByClassName('errors')[0].innerHTML += error + '<br>';
+									});
+							}
+						}
+					}
+				} else {
+					console.warn('Service Worker registration not found');
+					document.getElementsByClassName('errors')[0].innerHTML += 'Service Worker registration not found<br>';
+				}
+			}).catch(function (error) {
+				console.error('Error getting Service Worker registration:', error);
+				document.getElementsByClassName('errors')[0].innerHTML += error + '<br>';
+			});
+		} else {
+			console.warn('Notification permission denied');
+		}
+	});
+}
+
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+	navigator.serviceWorker.register('https://gacelabs.github.io/mcgi-mass-indoc-invite/props/js/service-worker.js')
+	.then(function (registration) {
+		console.log('Service Worker registered with scope:', registration.scope);
+		setTimeout(() => {
+			registration.update(); // Ensure the service worker is up to date
+		}, 333);
+	}).catch(function (error) {
+		console.error('Service Worker registration failed:', error);
+	});
+}
+
+function showNotification(title, body, redirectUrl) {
+	if (baptismDate == false) {
+		if (("Notification" in window) == false) {
+			console.warn("This browser does not support desktop notification");
+			return;
+		}
+
+		try {
+			if (('serviceWorker' in navigator && 'PushManager' in window) && mobileCheck()) {
+				reqNotification(title, body, redirectUrl);
+			} else {
+				var options = {
+					body: body,
+					icon: '/mcgi-mass-indoc-invite/props/images/logo.png'
+				};
+				// Check if the user has granted permission to show notifications
+				if (Notification.permission === "granted") {
+					// If permission is granted, create a notification
+					var notification = new Notification(title, options);
+
+					notification.onclick = function (event) {
+						event.preventDefault(); // Prevent the browser from focusing the Notification's tab
+						window.open(redirectUrl, '_blank');
+						notification.close();
+					};
+				} else if (Notification.permission !== "denied") {
+					// If permission has not been denied, request permission
+					Notification.requestPermission().then(function (permission) {
+						if (permission === "granted") {
+							var notification = new Notification(title, options);
+
+							notification.onclick = function (event) {
+								event.preventDefault(); // Prevent the browser from focusing the Notification's tab
+								window.open(redirectUrl, '_blank');
+								notification.close();
+							};
+						}
+					});
+				} else {
+					if (window.location.host.indexOf('local.') < 0) {
+						console.warn('Notifications is off, allow it and reload the page.');
+						alert('Notifications is off, allow it and reload the page.');
+					} else {
+						console.warn('Notifications not allowed for local environment.');
+					}
+				}
+			}
+		} catch (error) {
+			console.warn(error);
+			document.getElementsByClassName('errors')[0].innerHTML += error + '<br>';
+			// alert(error);
+		}
+	}
+}
+
+function openInNewTab(ui) {
+	if (ui) {
+		if (mobileCheck()) {
+			// console.log(ui.tagName, ui.textContent, ui.classList.value);
+			var sPrepend = '';
+			if (ui.classList.value == 'locale') sPrepend = 'MCGI ';
+			var url = 'https://www.google.com/maps/dir/Your+location/' + encodeURIComponent(sPrepend + ui.textContent);
+			window.open(url, '_blank').focus();
+		} else {
+			var xhr = new XMLHttpRequest();
+			xhr.open('GET', 'https://ipinfo.io/json', true);
+			xhr.onload = function () {
+				if (xhr.status >= 200 && xhr.status < 300) {
+					var data = JSON.parse(xhr.responseText);
+					var loc = data.loc.split(',');
+					var latitude = loc[0], longitude = loc[1];
+
+					var sPrepend = '';
+					if (ui.classList.value == 'locale') {
+						sPrepend = 'MCGI ';
+					}
+					// var url = `https://www.google.com/maps/dir/${latitude},${longitude}/` + encodeURIComponent(sPrepend + ui.textContent);
+					var url = `https://www.google.com/maps/dir/?api=1&origin=${latitude},${longitude}&destination=` + encodeURIComponent(sPrepend + ui.textContent) + '&avoid=ferries';
+					window.open(url, '_blank').focus();
+				} else {
+					console.warn('Error fetching coordinates:', xhr.statusText);
+				}
+			};
+			xhr.onerror = function () {
+				console.warn('Request failed!');
+			};
+			xhr.send();
+		}
+	}
+}
---
+++ props/js/script.js
@@ -0,0 +1,220 @@
+var intervalCount;
+var currentStartDate = localStorage.getItem('currentStartDate') == null ? new Date(new Date('2024-04-22').setHours(19, 0, 0, 0)) : new Date(localStorage.getItem('currentStartDate'));
+var currentEndDate = null;
+var sessionCount = 0, untilResetCount = 0;
+var notificationStartSoon = false, baptismDate = false, onGoing = false;
+var specificYoutubeChannel = 'https://m.youtube.com/@MCGIChannel';
+var specificFacebookChannel = 'https://m.facebook.com/MCGI.org';
+var sTuneIn = 'Tune-in tomorrow';
+var setCurrentDateTime = function (sDate) {
+	var now = new Date();
+	var hours = now.getHours();
+	var minutes = now.getMinutes();
+	var seconds = now.getSeconds();
+	sDate.setHours(hours);
+	sDate.setMinutes(minutes);
+	sDate.setSeconds(seconds);
+	return sDate;
+}
+
+var todaysDate = new Date();
+
+/* start of "for testing purposes" */
+	/* this current date */
+	// var todaysDate = setCurrentDateTime(new Date('2024-06-28'));
+	// var todaysDate = setCurrentDateTime(new Date('2024-07-04'));
+	/* program time adjustments */
+	// todaysDate = new Date(new Date(todaysDate).setHours(24, 0, 0, 0));
+/* end of "for testing purposes" */
+
+var todaysProgramStart = new Date(new Date(todaysDate).setHours(19, 0, 0, 0));
+var todaysProgramEnd = new Date(new Date(todaysDate).setHours(21, 0, 0, 0));
+var nextProgramStart = new Date(new Date(todaysProgramStart).getTime() + 1 * 24 * 60 * 60 * 1000);
+/* is morning? */
+var isMorning = function (todaysDate) {
+	return todaysDate.getTime() < todaysProgramStart.getTime();
+}
+/* program just started */
+var isOngoing = function (todaysDate) {
+	return todaysDate.getTime() >= todaysProgramStart.getTime() && todaysDate.getTime() <= todaysProgramEnd.getTime();
+}
+
+function setCurrentSessionCount() {
+	sessionCount = 0;
+	var thisDate = new Date(currentStartDate);
+	while (thisDate <= todaysProgramStart) {
+		var dayOfWeek = thisDate.getDay();
+		if (dayOfWeek !== 6 && dayOfWeek !== 0) { // Exclude Saturday (6) and Sunday (0)
+			sessionCount++;
+		}
+		thisDate.setDate(thisDate.getDate() + 1); // Move to the next day
+	}
+}
+
+function setEventDateTimeSession(todaysDate) {
+	var dEnd = (new Date(currentStartDate).setDate(new Date(currentStartDate).getDate() + 18)); // calculate end date including weekends
+	currentEndDate = new Date(new Date(dEnd).setHours(8, 0, 0, 0));
+	// console.log(monthsCount, currentStartDate, currentEndDate, todaysDate);
+	setCurrentSessionCount();
+
+	var formattedDate = formatDateToFJY(todaysDate);
+	var dateTextContent = formattedDate;
+	if (todaysDate.getMonth() != 4) {
+		dateTextContent = addDotAfterThirdCharacter(formattedDate);
+	}
+
+	document.getElementsByClassName("date-value")[0].textContent = dateTextContent;
+	var sWeekDay = new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(todaysDate);
+	document.getElementsByClassName('weekday')[0].textContent = sWeekDay;
+}
+
+function setMassBaptism() {
+	document.getElementsByClassName("arial-fnt")[0].innerHTML = 'MASS BAPTISM';
+	document.querySelector('address .locale').innerHTML = '<span class="locale-name">MCGI Chapel</span>';
+	document.getElementsByClassName('locale-address')[0].textContent = '189 MacArthur Hwy, Apalit, 2016 Pampanga';
+	document.querySelector(".info-sess .sessions").style.display = 'none';
+	document.querySelector(".info-sess .social-medias").style.display = 'none';
+	document.querySelector(".daytime .weektime").innerHTML = '8 AM PHT';
+}
+
+function nextSession(day) {
+	if (day.getDay() === 6 || day.getDay() === 0) {
+		// Got to weekdays when next day is Saturday (6) or Sunday (0)
+		do {
+			day.setDate(day.getDate() + 1); // Move to the next day
+		} while (day.getDay() === 0 || day.getDay() === 6);
+	}
+	return day;
+}
+
+function setTuneInStatus(callBack) {
+	var isWeekend = todaysDate.getDay() === 6 || todaysDate.getDay() === 0;
+	onGoing = false;
+	var dayCount = sessionCount;
+
+	if (isOngoing(todaysDate)) {
+		sTuneIn = 'Session on going...';
+		onGoing = true;
+	} else { /* not yet started */
+		if (isMorning(todaysDate)) {
+			if (sessionCount % 14 === 0) {
+				sTuneIn = 'Last session tune-in tonight';
+			} else if (sessionCount % 15 === 0) { /* last session day */
+				sTuneIn = 'Doctrine Acceptance';
+			} else {
+				sTuneIn = 'Tune-in tonight';
+			}
+		} else { /* either weekends, fridays or weekday */
+			if (sessionCount % 14 === 0) {
+				todaysProgramStart = new Date(new Date(nextProgramStart).setHours(8, 0, 0, 0));
+				todaysProgramEnd = new Date(new Date(nextProgramStart).setHours(12, 0, 0, 0));
+				dayCount = dayCount + 1;
+				setMassBaptism();
+				sTuneIn += ' morning';
+			} else {
+				if (isWeekend || sessionCount % 5 === 0) { /* every weekends or fridays */
+					sTuneIn = 'Tune-in Monday';
+				}
+				nextProgramStart = nextSession(new Date(nextProgramStart));
+				todaysProgramStart = new Date(new Date(nextProgramStart).setHours(19, 0, 0, 0));
+				todaysProgramEnd = new Date(new Date(nextProgramStart).setHours(21, 0, 0, 0));
+			}
+
+			console.clear();
+			console.log("\nCurrent session start date:", currentStartDate, "\nCurrent session end date:", currentEndDate);
+			console.log("\nCurrent session count:", sessionCount, "\nCurrent date:", todaysDate, "\n", "\nProgram starts:", todaysProgramStart, "\nProgram ends:", todaysProgramEnd, "\n\nTomorrow Program starts:", nextProgramStart);
+
+			setEventDateTimeSession(nextProgramStart);
+			clearInterval(intervalCount);
+			intervalCount = setInterval(function () {
+				updateEventCountdown(todaysProgramStart);
+			}, 1000);
+		}
+	}
+	
+	document.getElementById("session-day").innerHTML = '<strong>Day ' + dayCount + ', ' + sTuneIn + '</strong>';
+	callBack();
+}
+
+function setSessionEvent() {
+	var monthsCount = countMonths(currentStartDate);
+	// console.log(currentStartDate);
+	if (monthsCount > 0) {
+		for (let index = 1; index < monthsCount; index++) {
+			var givenDate = new Date(currentStartDate);
+			var dateAfter28Days = addDaysToDate(givenDate, 28);
+			currentStartDate = new Date(dateAfter28Days);
+		}
+		localStorage.setItem('currentStartDate', currentStartDate);
+	}
+
+	setEventDateTimeSession(todaysDate);
+
+	if (sessionCount % 15 === 0) {
+		// mass baptist day at 8am
+		baptismDate = true;
+		setMassBaptism();
+		todaysProgramStart = new Date(new Date(todaysProgramStart).setHours(8, 0, 0, 0));
+		todaysProgramEnd = new Date(new Date(todaysProgramEnd).setHours(12, 0, 0, 0));
+		var givenDate = new Date(nextProgramStart);
+		var dateAfter10Days = addDaysToDate(givenDate, 10);
+		nextProgramStart = new Date(dateAfter10Days);
+	} else {
+		if (sessionCount % 14 === 0) {
+			nextProgramStart = new Date(new Date(nextProgramStart).setHours(8, 0, 0, 0));
+		}
+		nextProgramStart = nextSession(new Date(nextProgramStart));
+	}
+
+	console.log("\nCurrent session start date:", currentStartDate, "\nCurrent session end date:", currentEndDate);
+	console.log("\nCurrent session count:", sessionCount, "\nCurrent date:", todaysDate, "\n", "\nProgram starts:", todaysProgramStart, "\nProgram ends:", todaysProgramEnd, "\n\nTomorrow Program starts:", nextProgramStart);
+	
+	intervalCount = setInterval(function () {
+		setTuneInStatus(function () {
+			updateEventCountdown(todaysProgramStart);
+		});
+	}, 1000);
+}
+
+function updateEventCountdown(start) {
+	todaysDate = setCurrentDateTime(todaysDate);
+	var now = new Date(todaysDate).getTime();
+	var distance = todaysProgramStart.getTime() - now;
+	// console.log(distance, new Date(now), start);
+
+	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	
+	if (onGoing == false) {
+		if (days != 0 || hours != 0 || minutes != 0 || seconds != 0) {
+			document.getElementById("countdown").innerHTML =
+				`<div class="countdown-segment"><span class="countdown-label">Days</span><span class="countdown-number days">${days}</span></div>` +
+				`<div class="countdown-segment"><span class="countdown-label">Hours</span><span class="countdown-number hours">${hours}</span></div>` +
+				`<div class="countdown-segment"><span class="countdown-label">Minutes</span><span class="countdown-number">${minutes}</span></div>` +
+				`<div class="countdown-segment"><span class="countdown-label">Seconds</span><span class="countdown-number">${seconds}</span></div>` +
+				`<div style="margin-top: -10px;"><span class="countdown-label">To go</span></div>`;
+		}
+	}
+
+	if (notificationStartSoon == false) {
+		notificationStartSoon = true;
+		if (days == 0 && hours == 0) {
+			showNotification('Starting soon - Standby', 'Watch via MCGI YouTube Channel', specificYoutubeChannel);
+		} else {
+			showNotification(sTuneIn, 'Watch via MCGI YouTube Channel', specificYoutubeChannel);
+		}
+	}
+	// console.log(untilResetCount);
+	/* untilResetCount++;
+	if (untilResetCount == 59) {
+		// console.info('reset ram every 60 seconds');
+		untilResetCount = 0;
+		clearInterval(intervalCount);
+		intervalCount = setInterval(function () {
+			setTuneInStatus();
+			updateEventCountdown(todaysProgramStart);
+		}, 1000);
+	} */
+}
