a0f66392b38d872eb7d8111973e247910e3e9889
Eddie <eddie@annix.com.au>
2024-10-28 23:03:46 +0800

    new sched push

--- props/js/script.js
+++ props/js/script.js
@@ -1,7 +1,7 @@
 var intervalCount;
 
 var forNewSched = new Date();
-if (forNewSched.getFullYear() == 2024 && forNewSched.getMonth() == 10) {
+if (forNewSched.getFullYear() == 2024 && forNewSched.getMonth() == 9) {
 	var currentStartDate = new Date(new Date('2024-10-29').setHours(19, 0, 0, 0));
 } else {
 	var currentStartDate = localStorage.getItem('currentStartDate') == null ? new Date(new Date('2024-04-22').setHours(19, 0, 0, 0)) : new Date(localStorage.getItem('currentStartDate'));
